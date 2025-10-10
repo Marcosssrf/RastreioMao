ac3f5239b55250bc8e9e14907f82b27cf99520c5
Marcos <marcosssrf@hotmail.com>
2025-10-09 22:38:14 -0300

    Nova estrutura + abrir camera!

--- src/main.py
+++ src/main.py
@@ -1,1 +1,22 @@
 import cv2
+
+
+def main():
+
+    capture = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = capture.read()
+        if not ret:
+            print("Erro :(")
+            break
+        cv2.imshow("Webcam!", frame)
+        # pressionar Q sai do loop
+        # ord transforma o caractere em um int
+        if cv2.waitKey(1) == ord("q"):
+            break
+    capture.release()
+    cv2.destroyAllWindows()
+
+
+main()
